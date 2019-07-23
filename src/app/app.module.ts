32a5b11515bc66e90b80702e704363a8325dc259
DarkEdiel96 <h.amiltone.diel@gmail.com>
2019-07-23 01:05:47 -0500

    Agregando material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListUsuariosComponent } from './list-usuarios/list-usuarios.component';
 
+//Angular Material 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list'; 
+
+
+
 import {UsuariosServices} from "./servicios/usuarios.service";
 
 @NgModule({
@@ -12,7 +19,11 @@ import {UsuariosServices} from "./servicios/usuarios.service";
     ListUsuariosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatListModule
   ],
   providers: [
     UsuariosServices
